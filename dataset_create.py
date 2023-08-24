8a88354e7f349d55ce230b097a65c8c01a3a82c3
Edmund <edmund@EdmundMacBook-Air.local>
2023-08-24 10:03:53 +0900

    replace gender

--- dataset_create.py
+++ dataset_create.py
@@ -6,47 +6,46 @@ def generate_data(n):
     data = []
     sample_date = datetime.strptime('2023-08-25', '%Y-%m-%d')
     ages = np.random.randint(20, 80, n)
-    genders = np.random.choice(['男性', '女性'], n)
+    # Change gender labels to English with lowercase letters
+    genders = np.random.choice(['male', 'female'], n)
     heights = np.random.uniform(150, 190, n)
     weights = np.random.uniform(45, 90, n)
     times = [sample_date + timedelta(hours=np.random.uniform(0, 24), minutes=np.random.uniform(0, 60)) for _ in range(n)]
     
-    # Initialize remaining_hours_before_eating based on the time of the day
     remaining_hours_before_eating = np.random.uniform(1, 20, n)
     for i in range(n):
         hour = times[i].hour
         if 7 <= hour < 11:  # Morning
             remaining_hours_before_eating[i] = np.random.uniform(7, 12)
         elif 11 <= hour < 14:  # Noon
-            remaining_hours_before_eating[i] = np.random.uniform(2, 3) if np.random.rand() > 0.5 else np.random.uniform(5, 6)
+            remaining_hours_before_eating[i] = np.random.uniform(4, 7) if np.random.rand() > 0.5 else np.random.uniform(5, 6)
         elif 14 <= hour < 18:  # Afternoon
             remaining_hours_before_eating[i] = np.random.uniform(3, 5)
         elif 18 <= hour < 24:  # Evening
-            remaining_hours_before_eating[i] = np.random.uniform(2, 4)
+            remaining_hours_before_eating[i] = np.random.uniform(2, 8)
     
-    # Calculate daily caloric needs
     daily_needs = np.array([compute_bmr(ages[i], genders[i], heights[i], weights[i]) for i in range(n)]) * 1.2
     calories_eaten = np.random.uniform(0, daily_needs)
-    next_meal_calories = np.random.uniform(300, 800, n)
+    next_meal_calories = np.random.uniform(300, 1000, n)
     
     can_eat = []
     for i in range(n):
         hour = times[i].hour
-        if 7 <= hour < 11:  # Morning
+        if 7 <= hour < 11:
             can_eat.append(int((calories_eaten[i] + next_meal_calories[i]) <= daily_needs[i] * 0.2))
-        elif 11 <= hour < 14:  # Noon
+        elif 11 <= hour < 14:
             can_eat.append(int((calories_eaten[i] + next_meal_calories[i]) <= daily_needs[i] * 0.5))
-        elif 14 <= hour < 18:  # Afternoon
+        elif 14 <= hour < 18:
             can_eat.append(int((calories_eaten[i] + next_meal_calories[i]) <= daily_needs[i] * 0.7))
-        elif 18 <= hour < 24:  # Evening
-            can_eat.append(int((calories_eaten[i] + next_meal_calories[i]) <= daily_needs[i] * 0.8))
-        else:  # Other times
+        elif 18 <= hour < 24:
+            can_eat.append(int((calories_eaten[i] + next_meal_calories[i]) <= daily_needs[i]))
+        else:
             can_eat.append(int((calories_eaten[i] + next_meal_calories[i]) <= daily_needs[i]))
     
     for i in range(n):
         data.append({
             '年齢': ages[i],
-            '性別': genders[i],
+            '性別': genders[i],  # Use the new English gender labels
             '身長': heights[i],
             '体重': weights[i],
             '今の時間帯': times[i].strftime('%Y-%m-%d %H:%M:%S'),
@@ -66,7 +65,8 @@ def write_to_csv(data, filename):
             writer.writerow(row)
 
 def compute_bmr(age, gender, height, weight):
-    if gender == '男性':
+    # Adjust BMR calculation to match the new gender labels
+    if gender == 'male':
         return (10 * weight) + (6.25 * height) - (5 * age) + 5
     else:
         return (10 * weight) + (6.25 * height) - (5 * age) - 161
--- lightGBM.py
+++ lightGBM.py
@@ -10,19 +10,23 @@ valid_data = pd.read_csv('validate_data.csv')
 
 # ラベルエンコーダの初期化
 le = LabelEncoder()
-# 性別を数値に変換
-train_data['性別'] = le.fit_transform(train_data['性別'])
-valid_data['性別'] = le.transform(valid_data['性別'])  
 
-# 今の時間帯から時間を抽出
+# Replacing Japanese gender tags with English ones
+train_data['性別'] = train_data['性別'].replace({"男性": "male", "女性": "female"})
+valid_data['性別'] = valid_data['性別'].replace({"男性": "male", "女性": "female"})
+
+# Convert genders to numerical values
+train_data['性別'] = le.fit_transform(train_data['性別'])
+valid_data['性別'] = le.transform(valid_data['性別'])
+
+# Extract hour from the timestamp
 train_data['hour'] = pd.to_datetime(train_data['今の時間帯']).dt.hour
 valid_data['hour'] = pd.to_datetime(valid_data['今の時間帯']).dt.hour
-
 
 train_data.drop('今の時間帯', axis=1, inplace=True)
 valid_data.drop('今の時間帯', axis=1, inplace=True)
 
-# 特徴量とターゲットを分割
+# Split features and target
 X_train = train_data.drop('食べれたか', axis=1)
 y_train = train_data['食べれたか']
 X_valid = valid_data.drop('食べれたか', axis=1)
@@ -31,7 +35,7 @@ y_valid = valid_data['食べれたか']
 lgb_train = lgb.Dataset(X_train, y_train)
 lgb_valid = lgb.Dataset(X_valid, y_valid, reference=lgb_train)
 
-# パラメータの設定
+# Parameter settings
 params = {
     'boosting_type': 'gbdt',
     'objective': 'binary',
@@ -44,24 +48,38 @@ params = {
 verbose_eval = 0
 num_round = 100
 
+bst = lgb.train(params, lgb_train, num_round, valid_sets=[lgb_valid], 
+                callbacks=[lgb.early_stopping(stopping_rounds=10, verbose=True), lgb.log_evaluation(verbose_eval)])
 
-#bst = lgb.train(params, lgb_train, num_round, valid_sets=[lgb_valid])
-bst = lgb.train(params, lgb_train, num_round, valid_sets=[lgb_valid], callbacks=[lgb.early_stopping(stopping_rounds=10, verbose=True),lgb.log_evaluation(verbose_eval)])
-
-
-# モデルを保存
+# Save the model
 pickle.dump(bst, open('parameter.sav', 'wb'))
 
-# 予測を実行
+# Make predictions
 y_pred = bst.predict(X_valid, num_iteration=bst.best_iteration)
-y_pred_binary = [1 if prob >= 0.5 else 0 for prob in y_pred]  
+y_pred_binary = [1 if prob >= 0.5 else 0 for prob in y_pred]
 
-# 精度を計算
+# Calculate accuracy
 accuracy = accuracy_score(y_valid, y_pred_binary)
 print(f"Validation Accuracy: {accuracy:.4f}")
 
+sample_data = {
+    '年齢': [25],
+    '性別': ['female'],
+    '身長': [168.0],
+    '体重': [60.0],
+    '今の時間帯': ['2023-08-25 19:30:00'],
+    '空いた時間': [6.5],
+    '食べたカロリー': [1000.0],
+    'これから食べるカロリー': [800.0]
+}
 
+df_sample = pd.DataFrame(sample_data)
+df_sample['性別'] = le.transform(df_sample['性別'])
+df_sample['hour'] = pd.to_datetime(df_sample['今の時間帯']).dt.hour
+df_sample.drop('今の時間帯', axis=1, inplace=True)
 
+pred_prob = bst.predict(df_sample, num_iteration=bst.best_iteration)
+pred_binary = 1 if pred_prob[0] >= 0.5 else 0
 
-
-
+print(f"Predicted Probability: {pred_prob[0]:.4f}")
+print(f"Predicted Class: {pred_binary}")
